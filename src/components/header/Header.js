e9678fb09a47a6f4474169a71737f03afe862e60
aemc <aemlcruz@gmail.com>
2019-03-09 14:28:17 -0500

    Add drawer and header boilerplate

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,36 +1,12 @@
-import React from 'react';
-import {withStyles} from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from "@material-ui/core/es/Toolbar/Toolbar";
-import IconButton from "@material-ui/core/es/IconButton/IconButton";
-import MenuIcon from '@material-ui/core/MenuItem';
-import Typography from "@material-ui/core/es/Typography/Typography";
-import AccountCircle from '@material-ui/icons/AccountCircle';
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
-  root: ''
+  root: ""
 });
 
 function Header() {
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton>
-          <IconButton>
-            <MenuIcon />
-          </IconButton>
-        </IconButton>
-        <Typography>
-          Snow HeatMap
-        </Typography>
-        <div>
-          <IconButton>
-            <AccountCircle />
-          </IconButton>
-        </div>
-      </Toolbar>
-    </AppBar>
-  )
+  return <div>header</div>;
 }
 
-export default withStyles(styles)(Header)
+export default withStyles(styles)(Header);
