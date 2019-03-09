c460e0e45a7e269a4082d08179e0aad8ffbd0198
aemc <aemlcruz@gmail.com>
2019-03-09 18:01:01 -0500

    Add heatmap colors

--- src/components/heatmap/Heatmap.js
+++ src/components/heatmap/Heatmap.js
@@ -1,4 +1,5 @@
 import React from "react";
+import classNames from "classnames";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
@@ -8,6 +9,7 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import { statuses, streets } from "../../constants/Constants";
+import {getRandomInt} from '../utils/utils'
 
 const styles = theme => ({
   root: {
@@ -22,8 +24,20 @@ const styles = theme => ({
     "&:nth-of_type(odd)": {
       backgroundColor: theme.palette.background.default
     }
+  },
+  cell1: {
+    backgroundColor: "red"
+  },
+  cell2: {
+    backgroundColor: "yellow"
   }
 });
+
+const colors = [
+  "#ef9a9a",
+  "#fff59d",
+  "#a5d6a7"
+];
 
 function Heatmap({ classes }) {
   return (
@@ -38,15 +52,25 @@ function Heatmap({ classes }) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {streets.map(street => {
+          {streets.map((street, index) => {
             return (
               <TableRow key={street.id}>
                 <TableCell>{street.name}</TableCell>
-                <TableCell/>
-                <TableCell/>
-                <TableCell/>
-                <TableCell/>
-                <TableCell/>
+                <TableCell
+                  style={{ backgroundColor: colors[getRandomInt(3)]}}
+                />
+                <TableCell
+                  style={{ backgroundColor: colors[getRandomInt(3)]}}
+                />
+                <TableCell
+                  style={{ backgroundColor: colors[getRandomInt(3)]}}
+                />
+                <TableCell
+                  style={{ backgroundColor: colors[getRandomInt(3)]}}
+                />
+                <TableCell
+                  style={{ backgroundColor: colors[getRandomInt(3)]}}
+                />
               </TableRow>
             );
           })}
