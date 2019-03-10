c1fb452802c4ee9a632b5f9ceef1b1d936776c78
Erin Ly <erinlydiep@gmail.com>
2019-03-10 01:05:14 -0500

    added text

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,6 +1,13 @@
 import React from "react";
 import plowimage from "./plowimage.png";
+import { Typography } from "@material-ui/core";
 
 export const Intro = () => {
-  return <img src={plowimage} />;
+  return (
+    <div>
+      <Typography>PlowPath</Typography>
+      <img src={plowimage}></img>
+      <Typography>Make sure there's a safe route for you.</Typography>
+    </div>
+    );
 };
