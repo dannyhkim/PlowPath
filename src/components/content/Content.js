6e808afd06616957cb390b2916d54748a3a23491
aemc <aemlcruz@gmail.com>
2019-03-09 22:55:27 -0500

    Update react

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -15,18 +15,18 @@ const styles = theme => ({
   content: {
     flexGrow: 1,
     padding: theme.spacing.unit * 3,
-    marginLeft: '20%',
-    marginRight: '20%'
+    marginLeft: "20%",
+    marginRight: "20%"
   }
 });
 
-function Content({ classes, children }) {
+const Content = ({ classes, children }) => {
   return (
     <main className={classes.content}>
       <div className={classes.toolbar} />
       {children}
     </main>
   );
-}
+};
 
 export default withStyles(styles)(Content);
