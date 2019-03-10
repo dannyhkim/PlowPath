a8654a4736fee18de235728dbd0f80e50d143f59
Erin Ly <erinlydiep@gmail.com>
2019-03-10 01:23:50 -0500

    styled title text

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -1,13 +1,32 @@
 import React from "react";
 import plowimage from "./plowimage.png";
 import { Typography } from "@material-ui/core";
+import { white } from "ansi-colors";
 
 export const Intro = () => {
   return (
-    <div>
-      <Typography>PlowPath</Typography>
-      <img src={plowimage}></img>
-      <Typography>Make sure there's a safe route for you.</Typography>
+    <div style={{textAlign: "center",
+      alignItems: "center",
+      justifyContent: "center",
+      color: "white",
+      fontWeight: "bold",
+      fontFamily: "Avenir"}}>
+      <Typography class="title"
+      style={{
+        display: "inline-flex",
+        fontSize: "100px"
+      }}>
+        PlowPath
+      </Typography>
+      <img style={{width: "600px", display: "inline-flex"}}
+      src={plowimage}></img>
+      <Typography class="description"
+      style={{
+        display: "inline-flex",
+        fontSize: "30px"
+      }}>
+      Make sure there's a safe route for you.
+      </Typography>
     </div>
     );
 };
